9f0bf854d239cd2d51cf4bfd471063fc2dd7a88e
Konstantin Tcholokachvili <tcholoka@gmail.com>
2013-05-02 16:50:13 +0200

    Removed 'if ... then ... else' and added 'while' for testing purpose

--- iotaml.js
+++ iotaml.js
@@ -4,7 +4,6 @@
 //WTF:
 // - infixs needs ';' but why
 // - exp in the end isn't in the grammar
-// - if ... the ... else doesn't works
 
 
 matchFailed = function (grammar, errorPos) {
@@ -34,48 +33,55 @@ matchFailed = function (grammar, errorPos) {
 }
 
 ometa IotaML {
-    LOWER_ID    = space* <lower+ ('_' lower+)*>:lower_id space*                                                                -> lower_id
-                | ('/' | "mod" | '*' | '+' | '-' | '^' | "::" | '=' | '@' | ">=" | '>' | "<>" | '<' | "<=" | ":=")+:lower_id   -> lower_id,
-    UPPER_ID    = space* <upper+ ('_' upper+)*>:upper_id space*                                                                -> upper_id,
-    INT         = space* <digit+>:d space*                                                                                     -> d,
-    FLOAT       = space* <digit+ '.' digit+>:f space*                                                                          -> f,
-    CHAR        = space* '\'' char:c '\'' space*                                                                               -> c,
+    // >> Constants <<
+    INT         = space* <digit+>:d                      space*                                                                -> d,
+    FLOAT       = space* <digit+ '.' digit+>:f           space*                                                                -> f,
+    CHAR        = space* '\'' char:c '\''                space*                                                                -> c,
     STRING      = space* '"'  (``""'' | '\'' | space | ~'"' anything:s)* '"' space*                                            -> s,
     CONSTANT    = FLOAT
                 | INT
                 | CHAR
                 | STRING,
+
+    // >> Identifiers <<
+    LOWER_ID    = ('/' | "mod" | '*' | '+' | '-' | '^' | "::" | '=' | '@' | ">=" | '>' | "<>" | '<' | "<=" | ":=")+:lower_id   -> lower_id
+                | space* <lower+ ('_' lower+)*>:lower_id space*                                                                -> lower_id,
+    UPPER_ID    = space* <upper+ ('_' upper+)*>:upper_id space*                                                                -> upper_id,
+    LONG_ID     = LOWER_ID:lower_id ('.' LOWER_ID)*                                                                            -> lower_id,
     LAB         = LOWER_ID:id -> id
-                | INT:i -> i,
+                | INT:i       -> i,
 
-    expression  = "if" expression:cond "then" expression:exp1 "else" expression:exp2                                           -> ['ifthenelse', cond, exp1, exp2]
-                | '(' expression:exp ')'                                                                                       -> exp
-                | '(' space* expression?:exp (space* ',' space* expression)* ')'                                               -> exp
-                | '{' exprow?:exp '}'                                                                                          -> ['record', exp]
-                | '[' space* expression?:exp (space* ',' space* expression)* ']'                                               -> exp
-                | '(' space* expression:exp space* ';' space* expression (space* ';' space* expression)* ')'                   -> exp
-                | "raise" expression:exp                                                                                       -> exp
-                | expression:exp "handle" match:m                                                                              -> ['handle', exp, m]
-                | expression:exp1 "and" expression:exp2                                                                        -> ['and', exp1, exp2]
-                | expression:exp1 "or" expression:exp2                                                                         -> ['or', exp1, exp2]
-                | expression:exp1 LOWER_ID:op expression:exp2                                                                  -> [op, exp1, exp2]
-                | CONSTANT:constant                                                                                            -> constant,
-    exprow      = LAB:lab space* '=' space* expression:exp (space* ',' space* exprow)*                                         -> ['lab', lab, exp],
+    // >> Expressions <<
+    expression  = '(' expression:exp ')'                                                                        -> exp
+                | '(' space* expression?:exp (space* ',' space* expression)* ')'                                -> exp
+                | '{' exprow?:exp '}'                                                                           -> ['record', exp]
+                | '[' space* expression?:exp (space* ',' space* expression)* ']'                                -> exp
+                | '(' space* expression:exp space* ';' space* expression (space* ';' space* expression)* ')'    -> exp
+                | "raise" expression:exp                                                                        -> exp
+                | "while" expression:exp1 "do" expression:exp2                                                  -> ['while', exp1, exp2]
+                | expression:exp1 LOWER_ID:op expression:exp2                                                   -> ['infix application', op, exp1, exp2]
+                | CONSTANT:constant                                                                             -> constant
+                | LONG_ID:l                                                                                     -> ['LID', l]
+                | expression:exp "handle" match:m                                                               -> ['handle', exp, m]
+                | expression:exp1 "and" expression:exp2                                                         -> ['and', exp1, exp2]
+                | expression:exp1 "or" expression:exp2                                                          -> ['or', exp1, exp2],
+    exprow      = LAB:lab space* '=' space* expression:exp (space* ',' space* exprow)*                          -> ['lab', lab, exp],
 
-    pattern     = space* '_':underscore space*                                                                                 -> underscore
-                | LOWER_ID:lower_id                                                                                            -> lower_id,
 
-    declaration = declaration:dec1 space* ';'? space* declaration:dec2                                                         -> [dec1, dec2]
-                | valbind:binding                                                                                              -> binding
-                | "exception" exnbind:except                                                                                   -> ['exception', except]
-                | "nonfix" LOWER_ID+:lower_id                                                                                  -> ['nonfix', lower_id]
-                | "infixr" space* <digit>?:n LOWER_ID+:lower_id                                                                -> ['infixr', n, lower_id]
-                | "infix"  space* <digit>?:n LOWER_ID+:lower_id                                                                -> ['infix', n, lower_id],
-    exnbind     = UPPER_ID:upper_id                                                                                            -> upper_id,
-    valbind     = space* pattern:id space* '=' space* expression:value                                                         -> ['binding', id, value],
-    program     = program:p1 space* ';'? space* program:p2                                                                     -> [p1, p2]
-                | declaration:dec                                                                                              -> dec
-                | expression:exp                                                                                               -> exp
+    // >> Patterns <<
+    pattern     = space* '_':underscore space*                                                                  -> underscore
+                | LOWER_ID:lower_id                                                                             -> lower_id,
+    declaration = declaration:dec1 space* ';'? space* declaration:dec2                                          -> [dec1, dec2]
+                | valbind:binding                                                                               -> binding
+                | "exception" exnbind:except                                                                    -> ['exception', except]
+                | "nonfix" LOWER_ID+:lower_id                                                                   -> ['nonfix', lower_id]
+                | "infixr" space* <digit>?:n LOWER_ID+:lower_id                                                 -> ['infixr', n, lower_id]
+                | "infix"  space* <digit>?:n LOWER_ID+:lower_id                                                 -> ['infix', n, lower_id],
+    exnbind     = UPPER_ID:upper_id                                                                             -> upper_id,
+    valbind     = space* pattern:id space* '=' space* expression:value                                          -> ['binding', id, value],
+    program     = program:p1 space* ';'? space* program:p2                                                      -> [p1, p2]
+                | declaration:dec                                                                               -> dec
+                | expression:exp                                                                                -> exp
 }
 
 
@@ -87,6 +93,7 @@ test = 'nonfix aaa;
         a = 5;
         {a = 25};
         1 + 2;
-        if 1 = 1 then 2 else 3;'
+        while a < 4 do
+            a := a + 1;'
 
 IotaML.matchAll(test, 'program', [], matchFailed)
